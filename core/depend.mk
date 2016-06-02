0fe89f93cf063eeba208f7494eecb09f71b431ec
shnarazk <shuji.narazaki@gmail.com>
2016-06-02 15:00:06 +0900

    store minisat 2.2

---
+++ core/depend.mk
@@ -0,0 +1,27 @@
+/home/nash/Sources/Arch/minisat/src/minisat/core/Solver.o /home/nash/Sources/Arch/minisat/src/minisat/core/Solver.or /home/nash/Sources/Arch/minisat/src/minisat/core/Solver.od /home/nash/Sources/Arch/minisat/src/minisat/core/Solver.op: /home/nash/Sources/Arch/minisat/src/minisat/core/Solver.cc \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Sort.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Vec.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/IntTypes.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/XAlloc.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/core/Solver.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Heap.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Alg.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/utils/Options.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/utils/ParseUtils.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/core/SolverTypes.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Map.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Alloc.h
+/home/nash/Sources/Arch/minisat/src/minisat/core/Main.o /home/nash/Sources/Arch/minisat/src/minisat/core/Main.or /home/nash/Sources/Arch/minisat/src/minisat/core/Main.od /home/nash/Sources/Arch/minisat/src/minisat/core/Main.op: /home/nash/Sources/Arch/minisat/src/minisat/core/Main.cc \
+ /home/nash/Sources/Arch/minisat/src/minisat/utils/System.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/IntTypes.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/utils/ParseUtils.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/utils/Options.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Vec.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/XAlloc.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/core/Dimacs.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/core/SolverTypes.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Alg.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Map.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Alloc.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/core/Solver.h \
+ /home/nash/Sources/Arch/minisat/src/minisat/mtl/Heap.h
